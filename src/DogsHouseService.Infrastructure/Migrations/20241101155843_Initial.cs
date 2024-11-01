f1865447444dc559dec8199acf96a17b647b0960
Hlib <pavlikgo78@gmail.com>
2024-11-01 18:00:34 +0200

    feat: add migration
    - add connection string in configs file

---
+++ src/DogsHouseService.Infrastructure/Migrations/20241101155843_Initial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DogsHouseService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "dogs",
+                columns: table => new
+                {
+                    name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    color = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    tail_length = table.Column<int>(type: "int", nullable: false),
+                    weight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dogs", x => x.name);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dogs_name",
+                table: "dogs",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dogs");
+        }
+    }
+}
